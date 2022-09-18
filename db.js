8bfc7048ee9cd39fcf738bb104d6864f4f4bed0e
sarkersh <1sarkm21@solent.ac.uk>
2022-09-18 01:10:03 +0100

    port

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const Covid19 = require('./model/worldometer');
+const dotenv = require('dotenv').config();
+
+
+
+const PORT = process.env.PORT
 
 const myDB = "mongodb+srv://sarkersh:Shakil2023@cluster0.fsnhzuj.mongodb.net/covid_19?retryWrites=true&w=majority"
 mongoose.connect(myDB, () => {
-    console.log('mongodb atlas connected')
+    console.log(`mongodb atlas connected at http://localhost:${PORT}`)
 }, e => console.log(e))
 
 module.exports = mongoose
